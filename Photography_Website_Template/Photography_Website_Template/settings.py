917dc24ca5392539d6c39af13a8f17bb5d943649
fozail-ahmed1 <fozailahmed02@gmail.com>
2022-11-02 01:49:02 +0530

    frist commit

--- Photography_Website_Template/Photography_Website_Template/settings.py
+++ Photography_Website_Template/Photography_Website_Template/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +137,13 @@ JS_ROOT = BASE_DIR /'static/js'
 STATIC_ROOT =BASE_DIR / 'staticfiles'
 IMAGES_ROOT = BASE_DIR /'static/Imagess'
 
+#extra 
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#activate django-heroku
+django_heroku.settings(locals())
